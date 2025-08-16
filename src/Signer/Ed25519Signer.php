aebd9b65b3a62121cc39e7097556934643f8173a
mitoop <me@mitoop.com>
2025-08-16 17:34:08 +0800

    adjust Ed25519Signer

--- src/Signer/Ed25519Signer.php
+++ src/Signer/Ed25519Signer.php
@@ -3,6 +3,7 @@
 namespace Mitoop\LaravelSignature\Signer;
 
 use Mitoop\LaravelSignature\Exceptions\InvalidArgumentException;
+use Mitoop\LaravelSignature\Key\KeyType;
 use SensitiveParameter;
 use SodiumException;
 
@@ -14,13 +15,9 @@ class Ed25519Signer extends EdDSASigner
      */
     public function sign(string $payload, #[SensitiveParameter] string $privateKey): string
     {
-        $rawSecretKey = base64_decode($privateKey, true);
+        $rawPrivateKey = $this->extractKeyFromPem($privateKey, KeyType::PRIVATE);
 
-        if ($rawSecretKey === false) {
-            throw new InvalidArgumentException('Invalid base64 secret key');
-        }
-
-        return base64_encode(sodium_crypto_sign_detached($payload, $rawSecretKey));
+        return base64_encode(sodium_crypto_sign_detached($payload, $rawPrivateKey));
     }
 
     /**
@@ -29,10 +26,7 @@ class Ed25519Signer extends EdDSASigner
      */
     public function verify(string $payload, #[SensitiveParameter] string $key, string $sign): bool
     {
-        $rawPublicKey = base64_decode($key, true);
-        if ($rawPublicKey === false) {
-            throw new InvalidArgumentException('Invalid base64 public key');
-        }
+        $rawPublicKey = $this->extractKeyFromPem($key);
 
         $rawSignature = base64_decode($sign, true);
         if ($rawSignature === false) {
@@ -40,5 +34,29 @@ class Ed25519Signer extends EdDSASigner
         }
 
         return sodium_crypto_sign_verify_detached($rawSignature, $payload, $rawPublicKey);
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    protected function extractKeyFromPem(string $pem, KeyType $keyType = KeyType::PUBLIC): string
+    {
+        $type = $keyType->value;
+        $pattern = sprintf(
+            '/-----BEGIN %s KEY-----(.+?)-----END %s KEY-----/s',
+            preg_quote($type, '/'),
+            preg_quote($type, '/')
+        );
+
+        if (! preg_match($pattern, $pem, $matches)) {
+            throw new InvalidArgumentException("Invalid PEM format for $type key");
+        }
+
+        $raw = base64_decode(trim($matches[1]), true);
+        if ($raw === false) {
+            throw new InvalidArgumentException("Cannot decode base64 content of $type key");
+        }
+
+        return substr($raw, -32);
     }
 }

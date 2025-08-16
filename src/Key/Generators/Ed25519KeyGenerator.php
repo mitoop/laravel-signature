da0d9ef453faae03480ed98e54d32784278241a5
mitoop <me@mitoop.com>
2025-08-16 17:39:01 +0800

    adjust Ed25519KeyGenerator

--- src/Key/Generators/Ed25519KeyGenerator.php
+++ src/Key/Generators/Ed25519KeyGenerator.php
@@ -16,28 +16,29 @@ class Ed25519KeyGenerator implements KeyGeneratorInterface
         $privateKey = sodium_crypto_sign_secretkey($keypair);
         $publicKey = sodium_crypto_sign_publickey($keypair);
 
-        $pkcs8PrivatePrefix = hex2bin(
-            '302e020100300506032b657004220420'
-        );
-
-        $x509PublicPrefix = hex2bin(
-            '302a300506032b6570032100'
-        );
-
         return [
-            $this->wrapKey($pkcs8PrivatePrefix.$privateKey, KeyType::PRIVATE),
-            $this->wrapKey($x509PublicPrefix.$publicKey),
+            $this->wrapKey($privateKey, KeyType::PRIVATE),
+            $this->wrapKey($publicKey),
         ];
     }
 
-    public function wrapKey(string $key, KeyType $keyType = KeyType::PUBLIC): string
+    public function wrapKey(string $rawKey, KeyType $keyType = KeyType::PUBLIC): string
     {
+        $prefix = $this->getDerPrefix($keyType);
+        $fullKey = $prefix.$rawKey;
+        $encoded = wordwrap(base64_encode($fullKey), 64, "\n", true);
         $type = $keyType->value;
 
-        $encodedKey = wordwrap(base64_encode($key), 64, "\n", true);
-
         return "-----BEGIN {$type} KEY-----\n"
-            .$encodedKey
+            .$encoded
             ."-----END {$type} KEY-----\n";
+    }
+
+    protected function getDerPrefix(KeyType $keyType): string
+    {
+        return match ($keyType) {
+            KeyType::PRIVATE => hex2bin('302e020100300506032b657004220420'), // PKCS#8 私钥
+            KeyType::PUBLIC => hex2bin('302a300506032b6570032100'), // X.509 公钥
+        };
     }
 }
